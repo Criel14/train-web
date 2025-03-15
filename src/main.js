31e86b2d6d5e41821506cafebc6dab0949f886bb
Criel14 <1298373079@qq.com>
2025-03-15 11:07:06 +0800

    添加axios拦截器，请求加上token，响应处理401

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import Antd from 'ant-design-vue';
-// import {notification} from "ant-design-vue";
+import Antd, {notification} from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 import * as Icons from '@ant-design/icons-vue';
 import axios from 'axios';
@@ -17,34 +16,44 @@ for (const i in icons) {
     app.component(i, icons[i]);
 }
 
-// axios拦截器
+// axios拦截器：拦截发出去的请求
 axios.interceptors.request.use(function (config) {
     console.log('请求参数：', config);
-    // const _token = store.state.member.token;
-    // if (_token) {
-    //     config.headers.token = _token;
-    //     console.log("请求headers增加token:", _token);
-    // }
+    const token = store.state.member.token;
+    if (token) {
+        config.headers.token = token;
+        console.log("请求headers增加token:", token);
+    }
     return config;
 }, error => {
     return Promise.reject(error);
 });
+
+// axios拦截器：拦截接收的响应结果
 axios.interceptors.response.use(function (response) {
     console.log('返回结果：', response);
     return response;
 }, error => {
     console.log('返回错误：', error);
-    // const response = error.response;
-    // const status = response.status;
-    // if (status === 401) {
-    //     // 判断状态码是401 跳转到登录页
-    //     console.log("未登录或登录超时，跳到登录页");
-    //     store.commit("setMember", {});
-    //     notification.error({description: "未登录或登录超时"});
-    //     router.push('/login');
-    // }
+    const response = error.response;
+    const status = response.status;
+    // 判断状态码是401 跳转到登录页
+    if (status === 401) {
+        console.log("未登录或登录超时，跳到登录页");
+        // 会员信息置为空对象
+        store.commit("setMember", {});
+        notification.error({description: "未登录或登录超时"});
+        // 延时跳转
+        setTimeout(() => {
+            router.push('/login');
+        }, 1000)
+        // 此处不再返回reject，直接返回一个空对象（或其他默认值）
+        return Promise.resolve({data: {}});
+    }
     return Promise.reject(error);
 });
+
 axios.defaults.baseURL = process.env.VUE_APP_SERVER;
+
 console.log('环境：', process.env.NODE_ENV);
 console.log('服务端：', process.env.VUE_APP_SERVER);
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -13,6 +13,9 @@
             :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
         >
           Content
+
+          // 测试按钮
+          <a-button type="primary" @click="test">Test Button</a-button>
         </a-layout-content>
       </a-layout>
     </a-layout>
@@ -22,6 +25,18 @@
 <script setup>
 import HeaderBar from "@/components/HeaderBar.vue";
 import SideBar from "@/components/SideBar.vue";
+import axios from "axios";
+import {notification} from "ant-design-vue";
+
+// 测试
+const test = () => {
+  axios.get("/member/member/count").then((response) => {
+    let data = response.data;
+    if (data.success) {
+      notification.success({description: "测试成功"});
+    }
+  })
+};
 </script>
 
 <style scoped>

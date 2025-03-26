9527a8bccb701cbb8d829ce6f8c79d5fc22d9186
Criel14 <1298373079@qq.com>
2025-03-26 23:17:37 +0800

    管理端：调整目录结构

--- train-admin/src/components/SideBar.vue
+++ train-admin/src/components/SideBar.vue
@@ -6,28 +6,35 @@
         mode="inline"
         :style="{ height: '100%', borderRight: 0 }"
     >
-      <a-sub-menu key="sub1">
+      <a-sub-menu key="train">
         <template #title>
               <span>
-                <user-outlined/>
-                主菜单
+                火车管理
               </span>
         </template>
-        <a-menu-item key="/station">
-          <router-link to="/station">车站管理</router-link>
+        <a-menu-item key="/train/station">
+          <router-link to="/train/station">车站管理</router-link>
         </a-menu-item>
-        <a-menu-item key="/train">
-          <router-link to="/train">车次管理</router-link>
+        <a-menu-item key="/train/train">
+          <router-link to="/train/train">车次管理</router-link>
         </a-menu-item>
-        <a-menu-item key="/train-station">
-          <router-link to="/train-station">火车车站管理</router-link>
+        <a-menu-item key="/train/train-station">
+          <router-link to="/train/train-station">火车车站管理</router-link>
         </a-menu-item>
-        <a-menu-item key="/train-carriage">
-          <router-link to="/train-carriage">火车车厢管理</router-link>
+        <a-menu-item key="/train/train-carriage">
+          <router-link to="/train/train-carriage">火车车厢管理</router-link>
         </a-menu-item>
-        <a-menu-item key="/train-seat">
-          <router-link to="/train-seat">火车座位管理</router-link>
+        <a-menu-item key="/train/train-seat">
+          <router-link to="/train/train-seat">火车座位管理</router-link>
         </a-menu-item>
+      </a-sub-menu>
+
+      <a-sub-menu key="batch">
+        <template #title>
+              <span>
+                定时任务管理
+              </span>
+        </template>
         <a-menu-item key="/batch/job">
           <router-link to="/batch/job">定时任务管理</router-link>
         </a-menu-item>
--- train-admin/src/router/index.js
+++ train-admin/src/router/index.js
@@ -2,62 +2,72 @@ import {createRouter, createWebHistory} from 'vue-router'
 import NotFoundComponent from "@/components/NotFoundComponent.vue";
 
 const routes = [
-  {
-    // 首页重定向到欢迎页
-    path: '/',
-    redirect: '/welcome'
-  },
-  {
-    path: '/',
-    name: 'main',
-    component: () => import('../views/MainView.vue'),
-    meta: {
-      loginRequire: true
+    {
+        // 首页重定向到欢迎页
+        path: '/',
+        redirect: '/welcome'
     },
-    children: [
-      {
-        path: 'welcome',
-        component: () => import('../views/main/WelcomeView.vue')
-      },
-      {
-        path: 'station',
-        component: () => import('../views/main/StationView.vue')
-      },
-      {
-        path: 'train',
-        component: () => import('../views/main/TrainView.vue')
-      },
-      {
-        path: 'train-station',
-        component: () => import('../views/main/TrainStationView.vue')
-      },
-      {
-        path: 'train-carriage',
-        component: () => import('../views/main/TrainCarriageView.vue')
-      },
-      {
-        path: 'train-seat',
-        component: () => import('../views/main/TrainSeatView.vue')
-      },
-      {
-        path: 'batch/job',
-        component: () => import('../views/main/JobView.vue')
-      },
-    ]
-  },
+    {
+        path: '/',
+        name: 'main',
+        component: () => import('../views/MainView.vue'),
+        meta: {
+            loginRequire: true
+        },
+        children: [
+            {
+                path: 'welcome',
+                component: () => import('../views/main/WelcomeView.vue')
+            },
+            {
+                path: 'train/',
+                children: [
+                    {
+                        path: 'station',
+                        component: () => import('../views/main/batch/StationView.vue')
+                    },
+                    {
+                        path: 'train',
+                        component: () => import('../views/main/batch/TrainView.vue')
+                    },
+                    {
+                        path: 'train-station',
+                        component: () => import('../views/main/batch/TrainStationView.vue')
+                    },
+                    {
+                        path: 'train-carriage',
+                        component: () => import('../views/main/batch/TrainCarriageView.vue')
+                    },
+                    {
+                        path: 'train-seat',
+                        component: () => import('../views/main/batch/TrainSeatView.vue')
+                    },
+                ]
+            },
+            {
+                path: 'batch/',
+                children: [
+                    {
+                        path: 'job',
+                        component: () => import('../views/main/train/JobView.vue')
+                    },
+                ]
+            },
+        ]
+    },
 
-  // 兜底路由：匹配所有未定义的路径
-  // 进入自定义404页面
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: NotFoundComponent,
-  },
+    // 兜底路由：匹配所有未定义的路径
+    // 进入自定义404页面
+    {
+        path: '/:pathMatch(.*)*',
+        name: 'NotFound',
+        component: NotFoundComponent,
+    },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+    history: createWebHistory(process.env.BASE_URL),
+    routes
 })
 
 export default router
